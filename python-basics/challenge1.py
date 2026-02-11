6eac9305efbfc21df992cf5be9119837111f8666
DylanMuriuki <dkinyua979@gmail.com>
2026-02-11 09:22:06 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university= "JKUAT"
+grade="A minus"
+course="Electrical & electronics engineering"
+cluster_points= 79
+
+print(f"My favourite university is {university}")
+print(f"I got an {grade} in my KCSE")
+print(f"My favourite course is {course}")
+print(f"I have {cluster_points} cluster points")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from Dylan")
+# city/town
+print("I'm from Ruiru")
+# my hobbies
+print("I like to play video games, watch shows,sleep,read novels and work out")
+
+age=25
+height=1.25
+weight=75
+
+print(f"I am {age} years old")
+print(f"I am {height} metres tall")
+print(f"And weigh {weight} kgs")
