e11bc51f06cadf24dffe700c27d41a6f6e475f4f
DylanMuriuki <dkinyua979@gmail.com>
2026-03-26 10:52:57 +0300

    inspire steam

---
+++ web-basics/JavaScript/lesson1.js
@@ -0,0 +1,22 @@
+// your code goes here
+name = "Dylan Muriuki"
+
+age = 19
+
+weight = 72.96
+
+fav_dish = "Chapati"
+
+f_number = 45
+s_number = 15
+sum_number = f_number + s_number
+diff_number = f_number - s_number
+product_number = f_number * s_number
+quotient_number = f_number / s_number
+
+console.log("Hello from " + name)
+console.log("I am " +age+ " years old")
+console.log("Sum of the numbers is " + sum_number)
+console.log("Difference of the numbers is " + diff_number)
+console.log("Product of the numbers is " + product_number)
+console.log("Quotient of the numbers is " + quotient_number)
---
+++ web-basics/JavaScript/lesson2.js
@@ -0,0 +1,20 @@
+// your code goes here
+
+/* This is
+a multiline
+comment*/
+
+//This is a single line comment
+
+age = 12
+
+while(age<= 20){
+    console.log(age)
+}
+
+if(age >= 18){
+    console.log("You are allowed to drive")
+}
+else{
+    console.log("You are not allowed to drive")
+}
