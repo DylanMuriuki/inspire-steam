3aa34e5d8623177d0aa1697481283d72a6baa63c
DylanMuriuki <dkinyua979@gmail.com>
2026-03-27 10:47:25 +0300

    inspire steam

---
+++ WEB/src/main.js
@@ -0,0 +1,15 @@
+function message(){
+    alert("FBI Open Up!");
+}
+var fnumber, snumber, sumnumbers
+function add(fnumber,snumber){
+    sumnumbers = fnumber + snumber
+    return sumnumbers;
+}
+
+function changeText(){
+    var btn = document.getElementById("myBtn")
+    var span = document.getElementById("output")
+    var input = document.getElementById("textbox")
+    textbox.style.color = "Green"
+}
